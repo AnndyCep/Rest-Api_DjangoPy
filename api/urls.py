573747d8424d489054625cbb8f1ee10c0de16cc6
Andres <hannsonandrey@hotmail.com>
2023-06-08 15:12:50 -0500

    Rest-api funciona correctamente, soporta los metodos GET, POST, PUT and delete y permite la lectura de los los datos

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import companiaView
 
 urlpatterns = [
-    path('companias/', companiaView.as_view(), name = "compania_list")
+    path('companias/', companiaView.as_view(), name = "compania_list"),
+    path('companias/<int:id>', companiaView.as_view(), name = "compania_proceso")
 ]
 
--- api/views.py
+++ api/views.py
@@ -1,7 +1,12 @@
 
+from typing import Any
+from django import http
 from django.http import JsonResponse
 from django.views import View
 from .models import company
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 
@@ -9,19 +14,48 @@ class companiaView(View):
     """ Esta clase es la que vamos a convertir nosotros en una vista 
         que sea capaz de procesar las respuestas"""
     
-    def get(self, request):
-        valor = list(company.objects.values())
-        if len(valor)>0:
-            datos = { 'mensajee' : "Exitoso" , "valor":valor}
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs) 
+    """Metodo que se ejecuta en cada peticion (despachar o enviar)"""
+    
+    def get(self, request, id=0):
+        if (id > 0):
+            compania = list(company.objects.filter(id=id).values())
+            if len(compania)>0:
+                compañias = compania[0]
+                datos =  { 'mensajee' : "Exitoso" , "compañias":compañias}
+            else:
+               datos = { 'mensajee' : "compañia no encontrada"} 
+            return JsonResponse(datos)
+        else:
+            compania = list(company.objects.values())
+            if len(compania)>0:
+                datos = { 'mensajee' : "Exitoso" , "compania":compania}
+            else:
+                datos = { 'mensajee' : "compañia no encontrada"}
+            return JsonResponse(datos)
+
+    def post(self, request):
+        jd = json.loads(request.body) # obtenemos la respuesta del body en formato json para guardarla en jd
+        company.objects.create(nombre = jd['nombre'], sitioWeb =jd['sitioWeb'],fondacion= jd['fondacion']) 
+        # Creamo a partir del modelo un nuevo elemento, como es un diccionario se realiza de la sigueitne forma. 
+        datos = { 'mensajee' : "Exitoso"}
+        return JsonResponse(datos)
+
+    def put(self, request , id):
+        jd = json.loads(request.body)
+        compania = list(company.objects.filter(id=id).values())
+        if len(compania)>0:
+            conpañias =company.objects.get(id=id)
+            conpañias.nombre = jd["nombre"]
+            conpañias.sitioWeb = jd["sitioWeb"]
+            conpañias.fondacion = jd["fondacion"]
+            conpañias.save()
+            datos = { 'mensajee' : "Exitoso"}
         else:
             datos = { 'mensajee' : "compañia no encontrada"}
         return JsonResponse(datos)
-
-    def post(self, request):
-        pass
-
-    def put(self, request):
-        pass
-
+    
     def delete(self, request):
         pass
